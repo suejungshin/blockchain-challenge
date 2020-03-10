1c5bf3af3603fc4b0173cb7a67e7b9d1ed309e68
Suejung Shin <suejungshin@Suejungs-MBP.attlocal.net>
2020-03-10 00:38:47 -0700

    Update to be able to make API call

--- cryptokitties.js
+++ cryptokitties.js
@@ -1,5 +1,4 @@
 var Web3 = require('web3');
-
 // "Web3.providers.givenProvider" will be set if in an Ethereum supported browser.
 var web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546');
 
@@ -11,15 +10,8 @@ const endpoint = `https://mainnet.infura.io/v3/${PROJECT_ID}`;
 const startingBlock = 6607985;
 const startingBlockHex = `0x${startingBlock.toString(16)}`;
 //const endingBlock = 7028323;
-const endingBlock = 6607986;
+const endingBlock = 6607990
 const endingBlockHex = `0x${endingBlock.toString(16)}`;
-
-// const data = JSON.stringify({
-//   jsonrpc: '2.0',
-//   method: 'eth_getBlockByNumber',
-//   params: [startingBlockHex,true],
-//   id: 1
-// })
 
 const data = JSON.stringify({
   jsonrpc: '2.0',
@@ -27,7 +19,8 @@ const data = JSON.stringify({
   params: [{
     address:'0x06012c8cf97BEaD5deAe237070F9587f8E7A266d', // address for cryptokitties
     topics: [
-    ], // 0x88c2a0bf is giveBirth
+      '0x0a5311bd2a6608f08a180df2ee7c5946819a649b204b554bb8e39825b2c50ad5' // topic for birth event
+    ],
     fromBlock: startingBlockHex,
     toBlock: endingBlockHex
   }],
@@ -42,14 +35,85 @@ axios({
     'Content-Type': 'application/json',
   }
 }).then((response) => {
-  //console.log(response.data.result);
-  let topic1 = response.data.result[0].topics[0]
-  let decoded = web3.eth.abi.decodeParameters(['uint256'], topic1)
-  console.log(decoded)
+ // console.log(response.data.result);
+   let inputs = [
+    {
+      "indexed": false,
+      "name": "owner",
+      "type": "address"
+    },
+    {
+      "indexed": false,
+      "name": "kittyId",
+      "type": "uint256"
+    },
+    {
+      "indexed": false,
+      "name": "matronId",
+      "type": "uint256"
+    },
+    {
+      "indexed": false,
+      "name": "sireId",
+      "type": "uint256"
+    },
+    {
+      "indexed": false,
+      "name": "genes",
+      "type": "uint256"
+    }
+  ]
+   let topics = ['0x0a5311bd2a6608f08a180df2ee7c5946819a649b204b554bb8e39825b2c50ad5']
+   console.log(web3.eth.abi.decodeLog(inputs, response.data.result[0].data, topics));
+
 })
 .catch((error) => {
   console.log(error);
 });
 
+
 //console.log(web3.eth.abi.decodeParameters(['string', 'uint256'], '0x000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000ea000000000000000000000000000000000000000000000000000000000000000848656c6c6f212521000000000000000000000000000000000000000000000000'));
 //> Result { '0': 'Hello!%!', '1': '234' }
+
+// console.log(web3.eth.abi.encodeEventSignature({
+// "constant": false,
+// "inputs": [{ "name": "_matronId", "type": "uint256" }],
+// "name": "giveBirth",
+// "outputs": [{ "name": "", "type": "uint256" }],
+// "payable": false,
+// "stateMutability": "nonpayable",
+// "type": "function"
+// }))
+
+console.log(web3.eth.abi.encodeEventSignature({
+  "anonymous": false,
+  "inputs": [
+    {
+      "indexed": false,
+      "name": "owner",
+      "type": "address"
+    },
+    {
+      "indexed": false,
+      "name": "kittyId",
+      "type": "uint256"
+    },
+    {
+      "indexed": false,
+      "name": "matronId",
+      "type": "uint256"
+    },
+    {
+      "indexed": false,
+      "name": "sireId",
+      "type": "uint256"
+    },
+    {
+      "indexed": false,
+      "name": "genes",
+      "type": "uint256"
+    }
+  ],
+  "name": "Birth",
+  "type": "event"
+}))
